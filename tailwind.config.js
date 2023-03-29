56b246855125d317d888c73c5eb958f817fa08dd
Aldrin1122 <0318-1190@lspu.edu.ph>
2023-03-29 08:52:24 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#51E9EF",
+        secondary: "#252b42",
+        lightgray: "#fafafa",
+        darkgray: '#5C5B5B',
+      },
+
+      fontFamily: {
+        Poppins: ['Poppins', 'san-serif']
+      },
+      fontSize: {
+        huge: 'clamp(3rem, 6vw, 5rem)',
+        huge2: 'clamp(2rem, 4vw, 3rem)'
+
+      }
+    },
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        ssm: '460px',
+        sm: '600px',
+        md: '728px',
+        lg: '984px',
+        xl: '1240px',
+      //  '2xl': '1496px',
+      },
+    }
+  },
+  plugins: [],
+}
+
